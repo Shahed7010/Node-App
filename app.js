65a794072a8909ac559f3e04633d309775348c33
shahed7010 <shahed7010@gmail.com>
2023-06-16 18:32:37 +0600

    route added

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
-const readline = require('readline');
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+const http = require('http');
+const fs = require('fs')
+const url = require('url')
+
+
+let html = fs.readFileSync('./src/views/index.html', 'utf-8')
+
+const server = http.createServer((request, response) => {
+    let { pathname, query } = url.parse(request.url, true);
+    // console.log(pathname, query)
+    if (request.url == '/' || pathname == '/home') {
+        response.writeHead(200, {
+            'Content-Type': 'text/html',
+        })
+        response.end(html.replace('{{%content%}}', 'You are in home page!'));
+    }
+    // response.end(html);
 });
 
-rl.question("please enter your name: ", (value) => {
-    console.log(`you entered ${value}`);
-    rl.close();
-});
-
-rl.on('close', () => {
-    console.log('interface closed');
-    process.exit();
-});
+server.listen('8000', '127.0.0.1', () => {
+    console.log('server has started');
+})
